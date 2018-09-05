58c033fdfdc6ca2d68fc8bc459894fd1f2dde7f9
Clebam <maitreclebam@gmail.com>
2018-09-06 00:54:01 +0300

    Added Remove-Centreon Host

---
+++ CentreonHost/Public/Remove-CentreonHost.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    .SYNOPSIS
+        Removes an existing host
+    .DESCRIPTION
+        Removes an existing host
+    .PARAMETER HostName
+        Corresponds to the name of the host
+    .EXAMPLE
+        Remove-CentreonHost -HostName "WebMdz01"
+    .NOTES
+        Author: Clebam
+        Version: 1.0
+#>
+function Remove-CentreonHost {
+    param (
+        [string] $HostName
+    )
+    Invoke-Centreon -CentreonSession $CentreonSession -Object HOST -Action DEL -Value $HostName
+}
