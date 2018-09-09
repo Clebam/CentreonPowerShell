49f320d9a8099a6bb6063155f08a3093176e7f74
Clebam <maitreclebam@gmail.com>
2018-09-09 17:34:46 +0300

    Handle Binary

---
+++ CentreonPowerShell/functions/Core/Get-CentreonBinary.ps1
@@ -0,0 +1,21 @@
+﻿<#
+    .SYNOPSIS
+        Sets credentials needed to call centreon
+    .DESCRIPTION
+        Sets credentials needed to call centreon
+    .EXAMPLE
+    Get-CentreonBinary
+
+    Returns the binary
+    .NOTES
+        Author: Clebam
+        Version: 1.0
+#>
+function Get-CentreonBinary {
+    [CmdletBinding()]
+    $Binary = Get-PSFConfigValue -FullName CentreonPowerShell.Centreon.Binary
+    if (-not $Binary) {
+        throw "Please use Set-CentreonBinary (-Register)"
+    }
+    $Binary
+}
---
+++ CentreonPowerShell/functions/Core/Set-CentreonBinary.ps1
@@ -0,0 +1,35 @@
+﻿<#
+    .SYNOPSIS
+        Sets binary used to call centreon CLAPI
+    .DESCRIPTION
+        Sets binary used to call centreon CLAPI
+    .PARAMETER Binary
+        Name or path to the binary of centreon (default: centreon)
+    .PARAMETER Register
+        Register the credentials to a configuration file
+    .PARAMETER Confirm
+        Prompts to confirm the action
+    .PARAMETER WhatIf
+        Performs the action as a test
+    .EXAMPLE
+    Set-CentreonBinary -Binary centreon -Register
+
+    Sets binary and stores then in a configuration file
+    .NOTES
+        Author: Clebam
+        Version: 1.0
+#>
+function Set-CentreonBinary {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Binary,
+        [switch] $Register
+    )
+
+    Set-PSFConfig -Module 'CentreonPowerShell' -Name 'Centreon.Binary' -Value $Binary
+    if ($Register) {
+        Register-PSFConfig -Module 'CentreonPowerShell' -Name 'Centreon.Binary'
+    }
+}
