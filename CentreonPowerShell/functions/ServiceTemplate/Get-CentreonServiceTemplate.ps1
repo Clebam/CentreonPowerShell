76633f3c3b29e610e0a9fb6d3990bfbd2f1dc192
Clebam <maitreclebam@gmail.com>
2018-09-10 20:30:16 +0300

    Moved

---
+++ CentreonPowerShell/functions/ServiceTemplate/Get-CentreonServiceTemplate.ps1
@@ -0,0 +1,31 @@
+﻿<#
+    .SYNOPSIS
+        Returns a list of service templates
+    .DESCRIPTION
+        Returns a list of service templates
+    .PARAMETER Filter
+        Simple string filter. No Regex (Beware, foo will return foo1, foo2, foo3)
+    .EXAMPLE
+        Get-CentreonServiceTemplate
+
+        Returns all the service templates of Centreon
+    .EXAMPLE
+        Get-CentreonServiceTemplate -Filter "Web"
+
+        Returns the service templates with "Web" in their names
+    .NOTES
+        Author: Clebam
+        Version: 1.0
+#>
+function Get-CentreonServiceTemplate {
+    [CmdletBinding()]
+    param (
+        [string] $Filter
+    )
+    if ($Filter) {
+        Invoke-Centreon -Object STPL -Action SHOW -Value $Filter
+    }
+    else {
+        Invoke-Centreon -Object STPL -Action SHOW
+    }
+}
