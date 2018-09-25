801a0ba4dc1a5879161c9def0b337ae76ffff18c
Clebam <maitreclebam@gmail.com>
2018-09-25 10:24:07 +0300

    New management functions

---
+++ CentreonPowerShell/functions/ImportExport/Export-CentreonConfiguration.ps1
@@ -0,0 +1,30 @@
+﻿<#
+    .SYNOPSIS
+        Exports a full centreon configuration to a file.
+    .DESCRIPTION
+        Exports a full centreon configuration to a file.
+    .PARAMETER Path
+        Name of the host
+    .PARAMETER Confirm
+        Prompts to confirm the action
+    .PARAMETER WhatIf
+        Performs the action as a test
+    .EXAMPLE
+        Export-CentreonConfiguration -Path "/tmp/centreoncfg.txt"
+
+        Exports the configuration stored to /tmp/centreoncfg.txt
+    .NOTES
+        Author: Clebam
+        Version: 1.0
+#>
+function Export-CentreonConfiguration {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path
+    )
+    if ($PSCmdlet.ShouldProcess($Path)) {
+        Invoke-Centreon -ManagementAction Export -Path $Path -NonCsvOutput | Out-File -FilePath $Path
+    }
+}
---
+++ CentreonPowerShell/functions/ImportExport/Import-CentreonConfiguration.ps1
@@ -0,0 +1,30 @@
+﻿<#
+    .SYNOPSIS
+        Imports a full centreon configuration from a file.
+    .DESCRIPTION
+        Imports a full centreon configuration from a file.
+    .PARAMETER Path
+        Name of the host
+    .PARAMETER Confirm
+        Prompts to confirm the action
+    .PARAMETER WhatIf
+        Performs the action as a test
+    .EXAMPLE
+        Import-CentreonConfiguration -Path "/tmp/centreoncfg.txt"
+
+        Imports the configuration stored in /tmp/centreoncfg.txt
+    .NOTES
+        Author: Clebam
+        Version: 1.0
+#>
+function Import-CentreonConfiguration {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path
+    )
+    if ($PSCmdlet.ShouldProcess($Path)) {
+        Invoke-Centreon -ManagementAction Import -Path $Path -NonCsvOutput
+    }
+}
