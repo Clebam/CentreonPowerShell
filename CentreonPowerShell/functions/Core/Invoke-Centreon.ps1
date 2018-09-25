df4d744ed88a37e59f535984af8568eafbfdf2f4
Clebam <maitreclebam@gmail.com>
2018-09-25 09:33:48 +0300

    Handled management function

--- CentreonPowerShell/functions/Core/Invoke-Centreon.ps1
+++ CentreonPowerShell/functions/Core/Invoke-Centreon.ps1
@@ -22,35 +22,51 @@
         Version: 1.0
 #>
 function Invoke-Centreon {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "OAV")]
     param(
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName = "OAV")]
         [ValidateNotNullOrEmpty()]
         [string] $Object,
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, ParameterSetName = "OAV")]
         [ValidateNotNullOrEmpty()]
         [string] $Action,
+        [Parameter(ParameterSetName = "OAV")]
         [ValidateNotNullOrEmpty()]
         [string] $Value,
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory, ParameterSetName = "Management")]
+        [ValidateSet("Import", "Export")]
+        [string] $ManagementAction,
+        [Parameter(ParameterSetName = "Management")]
+        [string] $Path,
         [switch] $NonCsvOutput,
         [ValidateSet("WebApi", "Binary")]
         $QueryType = (Get-CentreonQueryType)
     )
+
     if ($QueryType -eq "Binary") {
         $CentreonBinary = Get-CentreonBinary
         $CentreonSession = Get-CentreonCredential
         $arguments = "-u $($CentreonSession.UserName) -p $($CentreonSession.Password)"
 
-        if ($Object) {
-            $arguments += " -o $Object"
+        if ($PSCmdlet.ParameterSetName -eq "OAV") {
+            if ($Object) {
+                $arguments += " -o $Object"
+            }
+            if ($Action) {
+                $arguments += " -a $Action"
+            }
+            if ($Value) {
+                $arguments += ' -v "' + $Value + '"'
+            }
         }
-
-        if ($Action) {
-            $arguments += " -a $Action"
-        }
-
-        if ($Value) {
-            $arguments += ' -v "' + $Value + '"'
+        elseif ($PSCmdlet.ParameterSetName -eq "Management") {
+            if ($ManagementAction -eq "Import") {
+                $arguments += " -i $Path"
+            }
+            elseif ($ManagementAction -eq "Export") {
+                $arguments += " -e"
+            }
         }
         $Process = Invoke-Process -Process $CentreonBinary -Argument $arguments
 
@@ -71,26 +87,31 @@ An error occured :
     Error Output : $($Process.StandardError.ReadToEnd())
 "@
         }
+
     }
     elseif ($QueryType -eq "WebApi") {
         $WebServer = Get-CentreonWebServer
         $ApiToken = Get-CentreonApiToken
         $URL = "$WebServer/centreon/api/index.php?"
 
-        if ($Value) {
-            $Body = @{
-                "action" = "$Action"
-                "object" = "$Object"
-                "values" = "$Value"
+        if ($PSCmdlet.ParameterSetName -eq "OAV") {
+            if ($Value) {
+                $Body = @{
+                    "action" = "$Action"
+                    "object" = "$Object"
+                    "values" = "$Value"
+                }
+            }
+            else {
+                $Body = @{
+                    "action" = "$Action"
+                    "object" = "$Object"
+                }
             }
         }
-        else {
-            $Body = @{
-                "action" = "$Action"
-                "object" = "$Object"
-            }
+        elseif ($PSCmdlet.ParameterSetName -eq "Management") {
+            throw "Not yet implement in Web API. Please use binary."
         }
-
         (Invoke-WebRequest -ContentType "application/json" -Uri "$($URL)action=action&object=centreon_clapi" -Method Post -Headers $ApiToken -Body (ConvertTo-Json $Body)).Content | ConvertFrom-Json | Select-Object -ExpandProperty Result
     }
 }
